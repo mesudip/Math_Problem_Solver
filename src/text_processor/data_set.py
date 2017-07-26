34d46615e06c6e25f80c1648b449401594ab7d51
mesudip <sudipbhattarai100@gmail.com>
2017-07-26 08:28:08 +0545

    more Pre Processing

--- src/text_processor/data_set.py
+++ src/text_processor/data_set.py
@@ -5,11 +5,13 @@ import sys
 import sqlite3
 import nltk
 from collections import Counter
+from text_processor import equation_processor
+from array import array
 
-#from text_processor.classifier import Classifier
+# from text_processor.classifier import Classifier
 
 
-def database_init(source='../../dataset/dataset.json', destination='../../dataset/dataset.sqlite3'):
+def database_init_sqlite(source='../../dataset/dataset.json', destination='../../dataset/dataset.sqlite3'):
     database = sqlite3.connect(destination)
     cursor = database.cursor()
     cursor.execute('DROP TABLE IF EXISTS dataset')
@@ -42,6 +44,38 @@ def database_init(source='../../dataset/dataset.json', destination='../../datase
     database.close()
 
 
+def database_preprocessed_init_sqlite(source='../../dataset/dataset_preprocessed.json',
+                                      destination='../../dataset/dataset_preprocessed.sqlite3'):
+    database = sqlite3.connect(destination)
+    cursor = database.cursor()
+    cursor.execute('DROP TABLE IF EXISTS dataset')
+    cursor.execute('''
+                        CREATE TABLE 
+                                dataset(
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    param_count INTEGER,
+                                    digits TEXT,
+                                    question TEXT,
+                                    equation TEXT,
+                                    solution TEXT,
+                                    verbs TEXT
+                                )
+                       ''')
+    json_data = json.loads(open(source).read())
+    for data in json_data:
+        data['iparam'] = len(data['digits'])
+        data['digits'] = str(data['digits'])
+        data['verbs'] = str(data['verbs'])
+    cursor.executemany('''
+            INSERT INTO dataset(question,equation,solution,param_count,digits,verbs) 
+            VALUES(:question,:equation,:solution,:iparam,:digits,:verbs)
+        ''', json_data)
+
+    database.commit()
+    cursor.close()
+    database.close()
+
+
 def read_data_sqlite(source='../../dataset/dataset.sqlite3'):
     database = sqlite3.connect(source)
     ans = database.execute('SELECT * FROM dataset')
@@ -58,7 +92,11 @@ def read_data_json(source='../../dataset/dataset.json'):
     return proper_data
 
 
-def get_preprocessed_data():
+def read_preprocessed_data_json(source='../../dataset/dataset_preprocessed.json'):
+    return json.loads(open(source).read())
+
+
+def _get_preprocessed_data():
     # the lemmatizing function to be used for converting all forms of verbs into same form.
     # eg. is, am , was --> be
     #     eats, ate   --> eat
@@ -66,37 +104,109 @@ def get_preprocessed_data():
 
     # read the json data : it's a list of dictionaries containing data.
     # each dictionary has 'question', 'equation' and 'solution'
-    json_data = [x for x in read_data_json() if (len(x['equation']) is 1) and (len(x['solution']) is 1)]
-    print(json_data)
-    input()
-
+    # we now take only those whose  equaiton and solution count is 1.
+    json_data = [x for x in read_data_json() if (len(x['equation']) is 1) and (
+        len(x['solution']) is 1)]  # TODO 'Make it to work with multiple equations and solutions'
 
     # for each questions in data set.
+    error_count = 0
+    best_data = []
+    all_verbs = set()
+    all_equations=set()
     for data in json_data:
+        # the solution is previously a list having only one element
+        # so change that
+        data['solution'] = data['solution'][0]
+
+        # tokenize the question by sentences
         sentences = nltk.sent_tokenize(data['question'])
+
+        # to store word tokens
+
         tokens = []
         for sentence in sentences:
+            # tokenize words in sentence
             words = nltk.word_tokenize(sentence)
+            # add the part of speech info to the tokenized word and append all of the words into the tokens list
             tokens.extend(nltk.pos_tag(words))
 
         # find verbs in the word list and make it's counter.
         data['verbs'] = Counter([lemmatize(verb[0], pos='v') for verb in tokens if verb[1].startswith('V')])
+        all_verbs.update(data['verbs'].keys())
+        try:
+            # now extract the digits in the question
+            digits = data['digits'] = tuple([float(token[0]) for token in tokens if token[1] == 'CD'])
+            _set = set(digits)
+            if len(_set) is not len(digits):
+                raise Exception("Duplicate number in question", "Number matching is not possible")
+            # then convert equation into general form.
+            data['equation'] = equation_processor.generalize(data['equation'][0], data[
+                'digits'])  # TODO 'Change it when we model multiple equation and solution'
+            best_data.append(data)
+            all_equations.add(data['equation'])
+        except Exception as e:
+            # error formating the data
+            # just print error informaiton and ignore the error
+            # print(e, file=sys.stderr)
+            # print(data, file=sys.stderr)
+            # print(tokens, file=sys.stderr)
+            # print()
+            error_count += 1
+            # remove the erronous data from the json list
+    print('\n\n')
+    print("Error data :", error_count)
+    print("Correct data:", len(best_data))
 
-        # now extract the digits in the question
-        digits = data['digits'] = tuple([token[0] for token in tokens if token[1] == 'CD'])
+    all_verbs=list(all_verbs)
+    all_verbs.sort()
+    all_equations=list(all_equations)
+    all_equations.sort()
 
-        for i in range(len(digits)):
-            for equation in data['equation']:
-                equation.replace(str(digits[i]),'N_%d'%i)
-                print(equation)
-            input()
+    return best_data,all_verbs,all_equations
+class net_feeder:
+    def __init__(self,all_verbs,all_equations):
 
-        print(data)
-    return json_data
+        # assign some key for each verb.
+        self.verb_index={}
+        for index,verb in enumerate(all_verbs):
+            self.verb_index[verb]=index
+
+        self.equation_index={}
+        for index, equation in enumerate(all_equations):
+            self.equation_index[equation]=index
+
+    def format_data_set(self,data_set):
+        feeds=[]
+        outputs=[]
+
+        for data in data_set:
+
+            feed=array((0.,)*len(self.verb_index))
+            output=array((0.,)*(len(self.equation_index)+1))
+
+            verbs_of_data=data['verbs']
+            for verb in verbs_of_data:
+                feed[self.verb_index[verb]] = verbs_of_data[verb]
+
+            output[self.equation_index[data['equation']]]=1.0
+            bits = [float(x) for x in bin(len(data['digits']))[2:]]
+            start=-1
+
+            for bit in bits:
+                output[start]=i
+                start-=1
+
+            feeds.append(feed)
+        return feeds
 
 
 if __name__ == "__main__":
-    get_preprocessed_data()
+    data,verbs,equations = _get_preprocessed_data()
+
+    json.dump(data, open('../../dataset/dataset_preprocessed.json', 'w'))
+    database_preprocessed_init_sqlite()
+    print(verbs)
+    print("Verb count:",len(verbs))
 # jsondata = 0
 # training_set = jsondata[:500]
 #
--- src/text_processor/equation_processor.py
+++ src/text_processor/equation_processor.py
@@ -1,2 +1,71 @@
 #!/usr/bin/python3
+import re
 
+
+def generalize(equation, input_numbers):
+    '''
+
+    :param equation: the equation to generalize .
+    :param input_numbers:  the numbers occuring in the question in serial order.
+    :return:
+    '''
+
+    # remove whitespaces altogether
+    equation = equation.replace(' ', '')
+
+    # replace all the variable names with X
+    format_words = re.sub(r'[a-zA-Z_]+[a-zA-Z0-9_]*', 'X', equation)
+
+    # split the equation from using =
+    v1, v2 = format_words.split('=')
+
+    # if a side of equation starts with bracket and ends with bracket, remove the bracket.
+    if v1[0] == '(' and v1[-1] == ')':
+        v1 = v1[1:-1]
+    if v2[0] == '(' and v2[-1] == ')':
+        v2 = v2[1:-1]
+
+    # combine the splitted parts
+    ans = v1 + '=' + v2
+
+    # the code below  could make you psyco. so avoid it.
+    tokens = []
+    last = 0
+    for index in re.finditer(r'[0-9.]+', ans):
+        tokens.append(ans[last:index.start(0)])
+        tokens.append(ans[index.start(0):index.end(0)])
+        last = index.end(0)
+    tokens.append(ans[last:])
+    tokens = [x for x in tokens if x != '']
+    numbers = [x for x in range(len(tokens)) if re.match('[0-9.]+', tokens[x]) is not None]
+
+    # make sure that the number of digits in equation equals that in the question.
+    if len(numbers) is not len(input_numbers):
+        raise Exception("All digit parameters not used", "Parameter Mapping Fault")
+    sorted(numbers)
+
+    # now map the numbers in the sentence to the ones in the equation by index.
+    for number_index in numbers:
+        try:
+            index = input_numbers.index(float(tokens[number_index]))
+        except Exception as e:
+            raise Exception("Cannot map input number to numbers in equaiton",
+                            "Input ->" + str(equation) + ',' + str(input_numbers), e)
+
+        tokens[number_index] = 'N' + str(index)
+
+    ans = ''
+    for a in tokens:
+        ans += a
+    return ans
+
+
+if __name__ == "__main__":
+    equation = 'X=(9.0+29.0)'
+    print(equation, '-->', generalize(equation, [9.0, 29.0]))
+
+    equation = 'cost_of_1_lollipop=90.0/120.0'
+    print(equation, '-->', generalize(equation, [120.0, 90.0]))
+
+    equation = 'cost_of_1_lollipop=90.0/120.0+A_BC_D+30.2+1/2'
+    print(equation, '-->', generalize(equation, [120.0, 90.0, 1, 2, 30.2]))
